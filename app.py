b1f53164ef403c56706ef5c8ea2777faf44e091d
S3n4t1n0 <josetm1002@gmail.com>
2025-02-14 10:50:12 -0500

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import os
 
 # Configurar API KEY
-os.environ["GOOGLE_API_KEY"] = "TU_CLAVE_API_AQUI"
+os.environ["GOOGLE_API_KEY"] = "AIzaSyB9ImlFo2TO-liWy7eyCNu3kZI6V1IQfRw"
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # Aplicar estilo oscuro con CSS
